f1e245576a962e07b1b18d51aad93679b76f65d0
Patryk Stępień <patryk.stepien@automotivedev.pl>
2020-03-20 23:11:04 +0100

    Refactored constans in main

--- TcpClient/TcpClient.cpp
+++ TcpClient/TcpClient.cpp
@@ -3,20 +3,24 @@
 #include <windows.h>
 #include "Socket.h"
 
+#define IP_ADDRESS "127.0.0.1"
+#define PORT 54000
+#define DLL_WS_VER MAKEWORD(2, 2)
+
 int main()
 {
    WSADATA wsaData;
 
-   int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+   int result = WSAStartup(DLL_WS_VER, &wsaData);
    if (result != NO_ERROR)
-      printf("Initialization error.\n");
+      printf("Winsock initialization error\n");
 
    Socket socket;
    if (socket.init(Socket::IpProtocol::IPV4, Socket::TxProtocol::TCP) == true)
    {
       std::cout << "Socket init\n";
    }
-   if (socket.connect("127.0.0.1", 54000) == true)
+   if (socket.connect(IP_ADDRESS, PORT) == true)
    {
       std::cout << "Socket connect\n";
    }
