fec029d69079e861b8b4b678dc539b4b7a8e21ff
Patryk.Stepien <215886@edu.p.lodz.pl>
2020-03-20 20:04:36 +0100

    Added first implementation of main function

--- TcpClient/TcpClient.cpp
+++ TcpClient/TcpClient.cpp
@@ -5,5 +5,23 @@
 
 int main()
 {
+   WSADATA wsaData;
+
+   int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+   if (result != NO_ERROR)
+      printf("Initialization error.\n");
+
+   Socket socket;
+   if (socket.init(Socket::IpProtocol::IPV4, Socket::TxProtocol::TCP) == true)
+   {
+      std::cout << "Socket init\n";
+   }
+   if (socket.connect("127.0.0.1", 4001) == true)
+   {
+      std::cout << "Socket connect\n";
+   }
+   socket.close();
+
+   WSACleanup();
    return 0;
 }
