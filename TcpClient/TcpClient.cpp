2e9bb3262a54cbfa8386cf4fc9b7e57df228df3b
Patryk Stępień <patryk.stepien@automotivedev.pl>
2020-03-21 16:47:54 +0100

    Added possibility to input ip or dns address and port and printing error message

--- TcpClient/TcpClient.cpp
+++ TcpClient/TcpClient.cpp
@@ -1,24 +1,32 @@
 ﻿#include <iostream>
+#include <string>
 #include <memory>
 #include <winsock2.h>
 #include <windows.h>
 #include "Socket.h"
 
-#define IP_ADDRESS "localhost"
-#define PORT 54000
 #define DLL_WS_VER MAKEWORD(2, 2)
+
+std::string getErrorMessage(void);
 
 int main()
 {
    WSADATA wsaData;
    std::unique_ptr<Socket> socket = nullptr;
+   std::string ipAddress;
+   uint16_t port;
 
-   int result = WSAStartup(DLL_WS_VER, &wsaData);
-   if (result != NO_ERROR)
+   if (WSAStartup(DLL_WS_VER, &wsaData) != NO_ERROR)
    {
       printf("Winsock initialization error\n");
       return -1;
    }
+
+   std::cout << "Enter ip address or DNS address: ";
+   std::cin >> ipAddress;
+
+   std::cout << "Enter port: ";
+   std::cin >> port;
 
    socket = std::make_unique<Socket>();
 
@@ -29,25 +37,58 @@ int main()
    else
    {
       std::cout << "Cannot initialiaze a socket\n";
-      std::cout << "Error #" << WSAGetLastError() << "\n";
+      std::cout << "Error: " << getErrorMessage() << "\n";
       WSACleanup();
       return -1;
    }
 
-   if (true == socket->connect(IP_ADDRESS, PORT))
+   if (true == socket->connect(ipAddress.c_str(), port))
    {
-      std::cout << "Socket connected to server [" << IP_ADDRESS << ", " << PORT << "]\n";
+      std::cout << "Socket connected to server [" << ipAddress << ", " << port << "]\n";
    }
    else
    {
-      std::cout << "Cannot connect to server [" << IP_ADDRESS << ", " << PORT << "]\n";
-      std::cout << "Error #" << WSAGetLastError() << "\n";
+      std::cout << "Cannot connect to server [" <<  ipAddress << ", " << port << "]\n";
+      std::cout << "Error: " << getErrorMessage() << "\n";
       socket->close();
+      std::cout << "Socket closed" << "\n";
       WSACleanup();
       return -1;
    }
 
    socket->close();
+   std::cout << "Socket closed" << "\n";
    WSACleanup();
    return 0;
+}
+
+std::string getErrorMessage(void)
+{
+   std::string rV;
+
+   switch (WSAGetLastError())
+   {
+      case WSAETIMEDOUT:
+      {
+         rV = "Connection timed out.";
+         break;
+      }
+      case WSAECONNREFUSED:
+      {
+         rV = "Connection refused. Probably wrong port";
+         break;
+      }
+      case WSAHOST_NOT_FOUND:
+      {
+         rV = "Host not found. Wrong ip address or DNS address";
+         break;
+      }
+      default:
+      {
+         rV = "Unsupported error";
+         break;
+      }
+   }
+
+   return rV;
 }
